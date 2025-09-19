a9f97ba7e87318262db86a6f3bc4a2b057b97f67
Sreelal pv <126940249+sreelal46@users.noreply.github.com>
2025-09-19 23:01:00 +0530

    Update navbar.tsx

--- components/main/navbar.tsx
+++ components/main/navbar.tsx
@@ -28,7 +28,7 @@ export const Navbar = () => {
             className="cursor-pointer"
           />
           <span className="hidden sm:block text-white font-bold tracking-wide">
-            I'm Sreelal
+            I&apos;m Sreelal
           </span>
         </Link>
 
